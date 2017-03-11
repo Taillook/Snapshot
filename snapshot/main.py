86ee15eec4974fb4a79ace7e685811e6f7d779aa
Taillook <jousoku@gmail.com>
2017-03-11 18:46:07 +0900

    add methods opt,take

--- snapshot/main.py
+++ snapshot/main.py
@@ -6,14 +6,40 @@ import time
 import subprocess as sp
 import sys
 
-def main(args=sys.argv[1:]):
-    delay_time = 1
+h = False  # show_help
+delay_time = 1
+path = "photo.jpg"  # image_path
 
+
+def opt(args):
+    global h, delay_time, path
+    if ("-h" in args):
+        h = True
+    if ("-d" in args):
+        delay_time = int(args[args.index("-d") + 1])
+    if ("-p" in args):
+        path = args[args.index("-p") + 1]
+
+
+def take():
+    global h, delay_time, path
     cap = cv2.VideoCapture(0)
     time.sleep(delay_time)
     ret, frame = cap.read()
-    path = "photo.jpg"
-    cv2.imwrite(path,frame)
-
+    cv2.imwrite(path, frame)
     cap.release()
     cv2.destroyAllWindows()
+
+
+def main(args=sys.argv[1:]):
+    global h, delay_time, path
+    opt(args)
+
+    if h:
+        print("help")
+    else:
+        take()
+
+
+if __name__ == '__main__':
+    main()
