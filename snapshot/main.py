101b7dfc9c27e30989f1ebb09e1c7ebad983c35d
Taillook <jousoku@gmail.com>
2017-03-12 21:59:31 +0900

    add opyion -o

--- snapshot/main.py
+++ snapshot/main.py
@@ -6,23 +6,26 @@ import time
 import subprocess as sp
 import sys
 
-h = False  # show_help
+h = False # show_help
+o = False # open_file
 delay_time = 1
 path = "photo.jpg"  # image_path
 
 
 def opt(args):
-    global h, delay_time, path
+    global h, o, delay_time, path
     if ("-h" in args):
         h = True
     if ("-d" in args):
         delay_time = int(args[args.index("-d") + 1])
     if ("-p" in args):
         path = args[args.index("-p") + 1]
+    if ("-o" in args):
+        o = True
 
 
 def take():
-    global h, delay_time, path
+    global delay_time, path
     cap = cv2.VideoCapture(0)
     time.sleep(delay_time)
     ret, frame = cap.read()
@@ -33,13 +36,16 @@ def take():
 
 
 def main(args=sys.argv[1:]):
-    global h, delay_time, path
+    global h, o, path
     opt(args)
 
     if h:
         print("help")
     else:
         take()
+
+    if o:
+        sp.call(["open", path])
 
 
 if __name__ == '__main__':
